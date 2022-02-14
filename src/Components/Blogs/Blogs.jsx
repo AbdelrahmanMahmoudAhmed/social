78d2ba5e4a97e34567760002669ced3f8bb9a640
AbdelrahmanMahmoudAhmed <abdelrahman.mahmoud.ahmed@gmail.com>
2022-02-14 10:48:46 +0200

    new update

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -12,12 +12,12 @@ import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
 
 
 
-function Blogs( { filter , kind }) {
+function Blogs({ filter, kind }) {
 
 
     let [start, setStart] = useState(0)
     let [end, setEnd] = useState(5)
-    
+
 
 
     const navBtns = (e) => {
@@ -47,7 +47,7 @@ function Blogs( { filter , kind }) {
             elements[elements.length - 1].classList.remove('hidden')
         }
 
-        window.scrollTo({top: 0})
+        window.scrollTo({ top: 0 })
 
     }
 
@@ -66,7 +66,7 @@ function Blogs( { filter , kind }) {
             }
 
             let currentPage = ((start / 5) + 2)
-            
+
             elements[currentPage].classList.add('active')
 
 
@@ -81,9 +81,9 @@ function Blogs( { filter , kind }) {
             }
 
             let currentPage = ((start / 5))
-           
+
             elements[currentPage].classList.add('active')
-            
+
 
         }
 
@@ -102,26 +102,27 @@ function Blogs( { filter , kind }) {
         }
 
 
-        window.scrollTo({top: 0})
+        window.scrollTo({ top: 0 })
 
     }
 
 
-    let forPost ;
-    let forPostP ;
-    if(kind === "post" ){
+    let forPost;
+    let forPostP;
+    if (kind === "post") {
         forPost = 'for-post container';
         forPostP = 'for-post-p';
-    }else{
+    } else {
         forPost = '';
         forPostP = '';
-        
+
     }
 
-    
-    let  { data: blogs, isLoading, errorMsg } = useFeach('http://localhost:4000/posts', kind , filter )
 
-   
+    let { data: blogs, isLoading, errorMsg } = useFeach('https://api.npoint.io/b0f45f64c3a8913d1e02/posts', kind, filter)
+    // let { data: blogs, isLoading, errorMsg } = useFeach('http://localhost:4000/posts', kind, filter)
+
+
     return (
         <div className='blogs'>
             {
@@ -132,12 +133,12 @@ function Blogs( { filter , kind }) {
 
 
                             <div className="blog-img-section" >
-                            <Link to={`/post/${blog.id}`} onClick={()=>window.scrollTo({top: 0})}>
-                                <div className="blogs-img-holder">
-                                    <div className="overlay"></div>
-                                    <img src={require(`../../${blog.postPic}`).default} alt="" />
-                                </div>
-                                </Link>    
+                                <Link to={`/post/${blog.id}`} onClick={() => window.scrollTo({ top: 0 })}>
+                                    <div className="blogs-img-holder">
+                                        <div className="overlay"></div>
+                                        <img src={require(`../../${blog.postPic}`)} alt="" />
+                                    </div>
+                                </Link>
                                 <div className="react">
                                     <div className="heart">
                                         <BsHeart />
@@ -145,14 +146,13 @@ function Blogs( { filter , kind }) {
                                     <div className="counter">{blog.reactCounter}</div>
                                 </div>
                             </div>
-                            <Link to={`/post/${blog.id}`} onClick={()=>window.scrollTo({top: 0})}>
-                            <h3 className="post-title">{blog.postTitle}</h3>
+                            <Link to={`/post/${blog.id}`} onClick={() => window.scrollTo({ top: 0 })}>
+                                <h3 className="post-title">{blog.postTitle}</h3>
                             </Link>
                             <div className="post-info">
                                 <div className="user-img" ><img src={require(`../../${blog.userPhoto}`).default} alt="" /></div>
-
                                 <div className="auther">
-                                   <Link to={`/author/${blog.userName}`} onClick={()=>window.scrollTo({top: 0})} ><span  className="name">{blog.userName}</span></Link> 
+                                    <Link to={`/author/${blog.userName}`} onClick={() => window.scrollTo({ top: 0 })} ><span className="name">{blog.userName}</span></Link>
                                     <span className="mark"></span>
                                 </div>
                                 <div className="date">
@@ -165,9 +165,9 @@ function Blogs( { filter , kind }) {
                                 </div>
                             </div>
                         </div>
-                        <h3 className='for-post-title' style={{display: kind === "post" ? "block" : "none"}}>{blog.pTitle} </h3>
-                        <p className={`post-contenet ${forPostP}`}>{kind === "post" ? blog.postContent :blog.postContent.slice( 0, 390 ) + '  ....' }</p>
-                        <div className="social-share" style={{display: kind === "post" ? "none" : "flex"}}>
+                        <h3 className='for-post-title' style={{ display: kind === "post" ? "block" : "none" }}>{blog.pTitle} </h3>
+                        <p className={`post-contenet ${forPostP}`}>{kind === "post" ? blog.postContent : blog.postContent.slice(0, 390) + '  ....'}</p>
+                        <div className="social-share" style={{ display: kind === "post" ? "none" : "flex" }}>
                             <span className="social">
                                 <FaFacebookF />
                                 <FaGooglePlusG />
@@ -185,7 +185,7 @@ function Blogs( { filter , kind }) {
             {isLoading && <div className="loading">Loading ...............</div>}
             {!blogs && !isLoading && !errorMsg && (<div className="no-posts"> No Posts</div>)}
             {errorMsg && (<div className="err"> {errorMsg}</div>)}
-            <div style={{display: kind === "post" ? "none" : "flex"}} className="transition">
+            <div style={{ display: kind === "post" ? "none" : "flex" }} className="transition">
                 <span onClick={goForword} className='hidden'><IoIosArrowBack /></span>
                 <span className='active' onClick={navBtns}  >1</span>
                 <span onClick={navBtns}>2</span>
--- src/Components/SideBar/Comps/PopularPosts/PopularPosts.jsx
+++ src/Components/SideBar/Comps/PopularPosts/PopularPosts.jsx
@@ -7,7 +7,8 @@ import './PopularPosts.css'
 function PopularPosts() {
 
 
-    let  { data , isLoading, errorMsg } = useFeach('http://localhost:4000/posts?_sort=reactCounter&_order=desc')
+    let { data, isLoading, errorMsg } = useFeach('https://api.npoint.io/b0f45f64c3a8913d1e02/posts?_sort=reactCounter&_order=desc')
+    // let { data, isLoading, errorMsg } = useFeach('http://localhost:4000/posts?_sort=reactCounter&_order=desc')
 
     return (
         <div className='popular-posts'>
@@ -15,35 +16,35 @@ function PopularPosts() {
                 <h3 className="title">POPULAR POST</h3>
                 <span></span>
             </div>
-            {data && data.slice(0 , 3).map((item) => (
+            {data && data.slice(0, 3).map((item) => (
                 <div className="pop-post" key={item.id}>
-                <div className="post-img">
-                <Link to={`/post/${item.id}`} onClick={()=>window.scrollTo({top: 0})}>
+                    <div className="post-img">
+                        <Link to={`/post/${item.id}`} onClick={() => window.scrollTo({ top: 0 })}>
 
-                    <img src={require(`../../../../${item.postPic}`).default} alt="" />
-                    </Link>
-
-                </div>
-                <div className="pop-content">
-                <Link to={`/post/${item.id}`} onClick={()=>window.scrollTo({top: 0})}>
-                    <h4 className="pop-title">{item.postTitle}</h4>
-                </Link>
-                    <div className="info-container">
-                    <Link to={`/author/${item.userName}`} onClick={()=>window.scrollTo({top: 0})} >
-                        <span className="auther">{item.userName}</span>
+                            <img src={require(`../../../../${item.postPic}`)} alt="" />
                         </Link>
-                        <Link to={`/postDate/${item.postDate}`}> <span className="date">{item.postDate}</span> </Link>
-                        
+
+                    </div>
+                    <div className="pop-content">
+                        <Link to={`/post/${item.id}`} onClick={() => window.scrollTo({ top: 0 })}>
+                            <h4 className="pop-title">{item.postTitle}</h4>
+                        </Link>
+                        <div className="info-container">
+                            <Link to={`/author/${item.userName}`} onClick={() => window.scrollTo({ top: 0 })} >
+                                <span className="auther">{item.userName}</span>
+                            </Link>
+                            <Link to={`/postDate/${item.postDate}`}> <span className="date">{item.postDate}</span> </Link>
+
+                        </div>
                     </div>
                 </div>
-            </div>
-            )) }
+            ))}
             {isLoading && <div className="loading">Loading ...............</div>}
             {!data && !isLoading && !errorMsg && (<div className="no-posts"> No Posts</div>)}
             {errorMsg && (<div className="err"> {errorMsg}</div>)}
-            
-           
-            
+
+
+
         </div>
     )
 }
